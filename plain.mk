826e0420110f0092d0bc2a7f252379cfdda0a364
Snuzzo <jrracinfan@gmail.com>
2014-10-05 18:01:46 -0400

    Add bootanimation

--- plain.mk
+++ plain.mk
@@ -2,8 +2,8 @@
 PRODUCT_RELEASE_NAME := Nexus7
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 800
+PRODUCT_COPY_FILES += \
+    vendor/plain/prebuilt/bootanimation/800.zip:system/media/bootanimation.zip
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/plain/config/common.mk)
