7ab1b14c2fe88ea070929fde7fcf61ebe38672d3
Snuzzo <jrracinfan@gmail.com>
2014-12-04 15:04:50 -0500

    Move from static to variables to incoporate aromazip function

--- aroma/aroma.mk
+++ aroma/aroma.mk
@@ -1,7 +1,11 @@
+AROMA_DEVICE_CONFIG := device/asus/grouper/aroma/aroma-device-config
+AROMA_DEVICE_SCRIPT := device/asus/grouper/aroma/aroma-device-script
+AROMA_DEVICE_FILES := device/asus/grouper/aroma/aroma_device_files
+
 PRODUCT_COPY_FILES += \
-	device/asus/grouper/aroma/aroma-device-config:aroma-device-config \
-	device/asus/grouper/aroma/aroma-device-script:aroma-device-script \
-	device/asus/grouper/aroma/aroma_device_files/deviceconfig.sh:aroma_device/deviceconfig.sh
+	$(AROMA_DEVICE_CONFIG):aroma-device-config \
+	$(AROMA_DEVICE_SCRIPT):aroma-device-script \
+	$(AROMA_DEVICE_FILES)/deviceconfig.sh:aroma_device/deviceconfig.sh
 
 #PRODUCT_COPY_FILES += \
 #	device/asus/grouper/aroma/aroma_device_files/<SOURCE-FILE>:aroma_device/<DEST-FILE>
